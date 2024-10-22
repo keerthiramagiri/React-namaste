f1f6b3e4d069502f18c4a28ff37f79c0c26b6b38
KeerthiRamagiri <ramagirikeerthi18@gmail.com>
2024-10-22 12:58:02 -0500

    updated nested structure

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 
 /**
  * <div id="parent">
- *     <div id="child">
+ *     <div id="child1">
+ *        <h1> I'm h1 tag</h1>
+ *        <h2> I'm h2 tag</h2>
+ *     </div>
+ *  <div id="child2">
  *        <h1> I'm h1 tag</h1>
  *        <h2> I'm h2 tag</h2>
  *     </div>
@@ -13,10 +17,14 @@
 const parent = React.createElement(
     "div", 
     {id:"parent"},
-    React.createElement("div", {id: "child"}, [
+    React.createElement("div", {id: "child1"}, [
         React.createElement("h1", {}, "I'm a h1 tag"), 
         React.createElement("h2", {}, "I'm a h2 tag"),
-    ])
+    ]),
+    React.createElement("div", {id: "child2"}, [
+        React.createElement("h1", {}, "I'm a h1 tag"), 
+        React.createElement("h2", {}, "I'm a h2 tag"),
+    ]),
 );
 console.log(parent); //object
 
