a9fe39d85805e9a5cada23ea1eb78845f27b1d19
KeerthiRamagiri <ramagirikeerthi18@gmail.com>
2024-10-31 15:23:12 -0500

    updated

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -9,7 +9,7 @@ const RestaurantCard = (props) => {
             <img 
             className ="res-logo"
             alt="res-logo"
-            src={CDN_URL+resData.info.cloudinaryImageId} />
+            src={CDN_URL+cloudinaryImageId} />
             <h3>{name}</h3>
             <h4>{cuisines.join(" , ")}</h4>
             <h4>{avgRating}</h4>
